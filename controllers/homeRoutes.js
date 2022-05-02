ff56d465b6c0666eafbc5d796faef5463847ab56
Jesse Comeau <97984389+JesseComeau@users.noreply.github.com>
2022-05-02 17:24:27 -0500

    Initial commit with basic routes and models established.

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const User = require('../models/User');
+
+router.get('/', async (req, res) => {
+  // Send the rendered Handlebars.js template back as the response
+  try {
+    const userData = await User.findAll({
+      attributes: { exclude: ['password'] },
+      order: [['name', 'ASC']],
+    });
+
+    const users = userData.map((project) => project.get({ plain: true }));
+
+    res.render('homepage', {
+      users,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
---
+++ models/Post.js
@@ -0,0 +1,32 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model {}
+
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        post_title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        post_message: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
+    },
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post',
+    }
+);
+
+module.exports = Post;
