50a47faf6b3d13714854a6b5e390a8c442d47354
Jesse Comeau <97984389+JesseComeau@users.noreply.github.com>
2022-06-02 11:00:12 -0500

    Formatted partials and removed login requirements for homepage.

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,28 +1,6 @@
 const router = require('express').Router();
 const { User, Post, Comment } = require('../../models');
 
-// router.get('/', async (req, res) => {
-//     // Send the rendered Handlebars.js template back as the response
-//     try {
-//       const dbPostData = await Post.findAll({
-//         order: [['id', 'DESC']],
-//         limit: 10,
-//         include: [{ model: Comment}],
-//       });
-
-//       const postData = dbPostData.map((post) =>
-//       post.get({ plain: true })
-//     );
-//       // res.status(200).json(postData);
-//       res.render('homepage', {
-//         postData,
-//         logged_in: req.session.logged_in,
-//       });
-//     } catch (err) {
-//       res.status(500).json(err);
-//     }
-//   });
-
 router.post('/', async (req, res) => {
   try {
     const commentData = await Comment.create({
@@ -36,22 +14,5 @@ router.post('/', async (req, res) => {
     console.log(err)
   }
 });
-
-// router.get('/dashboard', async (req, res) => {
-//   try {
-//     const userData = await User.findByPk(req.params.id, {
-//       include: [{ model: Post}]
-//     });
-
-//     if (!userData) {
-//       res.status(404).json({ message: 'No id found with that id!' });
-//       return;
-//     }
-
-//     res.status(200).json(userData);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
 
 module.exports = router;
